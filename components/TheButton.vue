fc98dd0aeca89986ec1d90db44d32a4b18d80e8c
Maxim Belkin <maximbe@ext.bershka.com>
2023-02-01 20:39:30 +0100

    [common-vue] feat: dummy btns

---
+++ components/TheButton.vue
@@ -0,0 +1,89 @@
+<template>
+  <button
+    class="button"
+    :class="{
+      red,
+      blue,
+      green,
+      orange
+    }"
+    @click="$emit('buttonClick')"
+  >
+    <slot>
+      Default text
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TheButton',
+  props: {
+    red: {
+      type: Boolean,
+      default: false
+    },
+    blue: {
+      type: Boolean,
+      default: false
+    },
+    green: {
+      type: Boolean,
+      default: false
+    },
+    orange: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 1rem;
+  width: 100%;
+  border: 0;
+  border-radius: 0.25rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &.red {
+    background-color: #f56565;
+    color: #fff;
+
+    &:hover {
+      background-color: #ed8936;
+    }
+  }
+
+  &.blue {
+    background-color: #4299e1;
+    color: #fff;
+
+    &:hover {
+      background-color: #3182ce;
+    }
+  }
+
+  &.green {
+    background-color: #48bb78;
+    color: #fff;
+
+    &:hover {
+      background-color: #38a169;
+    }
+  }
+
+  &.orange {
+    background-color: #ed8936;
+    color: #fff;
+
+    &:hover {
+      background-color: #dd6b20;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,73 @@
 <template>
-  <Tutorial />
+  <div class="grid">
+    <div class="grid-item">
+      <TheButton
+        red
+        @buttonClick="logRed"
+      >
+        Red Button
+      </TheButton>
+    </div>
+    <div class="grid-item">
+      <TheButton
+        blue
+        @buttonClick="logBlue"
+      >
+        Blue Button
+      </TheButton>
+    </div>
+    <div class="grid-item">
+      <TheButton
+        green
+        @buttonClick="logGreen"
+      >
+        Green Button
+      </TheButton>
+    </div>
+    <div class="grid-item">
+      <TheButton
+        orange
+        @buttonClick="logOrange"
+      >
+        Orange Button
+      </TheButton>
+    </div>
+  </div>
 </template>
 
 <script>
+import TheButton from '~/components/TheButton.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    TheButton
+  },
+  methods: {
+    logRed () {
+      // eslint-disable-next-line no-console
+      console.log('%c Red Button Clicked', 'background: red')
+    },
+    logBlue () {
+      // eslint-disable-next-line no-console
+      console.log('%c Blue Button Clicked', 'background: blue')
+    },
+    logGreen () {
+      // eslint-disable-next-line no-console
+      console.log('%c Green Button Clicked', 'background: green')
+    },
+    logOrange () {
+      // eslint-disable-next-line no-console
+      console.log('%c Orange Button Clicked', 'background: orange')
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  margin: 1rem;
+}
+</style>
